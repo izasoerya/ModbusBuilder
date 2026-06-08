6071d9af694faff83d896f42b62fd337d48d8c80
izasoerya <ihza.surya@gmail.com>
2026-06-08 17:01:44 +0700

    feat: refactor into head and body file

---
+++ src/SensorBuilder.cpp
@@ -0,0 +1,146 @@
+#include "SensorBuilder.h"
+
+ModbusRTUBuilder::ModbusRTUBuilder(HardwareSerial &serial, uint8_t enPin)
+    : _base(ModbusBase()), _ss(serial), _enablePin(enPin) {}
+
+ModbusRTUBuilder::~ModbusRTUBuilder() = default;
+
+uint8_t ModbusRTUBuilder::connect()
+{
+    callbackPin() = _enablePin;
+    pinMode(_enablePin, OUTPUT);
+    digitalWrite(_enablePin, LOW);
+
+    _node.preTransmission(preTransmission);
+    _node.postTransmission(postTransmission);
+    _node.begin(_base.slaveId, _ss);
+
+    uint8_t result = _node.readHoldingRegisters(0x00, 1);
+    if (result == _node.ku8MBSuccess)
+    {
+        return true;
+    }
+
+    // TODO: PARSE ERROR / VERBOSE
+    return result;
+}
+
+uint16_t ModbusRTUBuilder::read(uint8_t responseIndex)
+{
+    if (responseIndex >= _base.lengthAddress)
+    {
+        return 0;
+    }
+
+    if (_base.functionCode == 0x03)
+    {
+        uint8_t result = _node.readHoldingRegisters(_base.address, _base.lengthAddress);
+        if (result == _node.ku8MBSuccess)
+        {
+            return _node.getResponseBuffer(responseIndex);
+        }
+    }
+    else if (_base.functionCode == 0x04)
+    {
+        uint8_t result = _node.readInputRegisters(_base.address, _base.lengthAddress);
+        if (result == _node.ku8MBSuccess)
+        {
+            return _node.getResponseBuffer(responseIndex);
+        }
+    }
+    return 0;
+}
+
+BuilderBaseModbus &ModbusRTUBuilder::setSlaveId(uint8_t slaveId)
+{
+    _base.slaveId = slaveId;
+    return *this;
+}
+BuilderBaseModbus &ModbusRTUBuilder::setAddress(uint8_t address)
+{
+    _base.address = address;
+    return *this;
+}
+
+BuilderBaseModbus &ModbusRTUBuilder::setFunctionCode(uint8_t functionCode)
+{
+    _base.functionCode = functionCode;
+    return *this;
+}
+
+BuilderBaseModbus &ModbusRTUBuilder::setLengthAddress(uint8_t lengthAddress)
+{
+    _base.lengthAddress = lengthAddress;
+    return *this;
+}
+
+ModbusBase ModbusRTUBuilder::getConfig()
+{
+    return _base;
+}
+
+MockModbusRTUBuilder::MockModbusRTUBuilder(HardwareSerial &serial, uint8_t enPin)
+    : _base(ModbusBase()), _ss(serial), _enablePin(enPin) {}
+
+MockModbusRTUBuilder::~MockModbusRTUBuilder() = default;
+
+uint8_t MockModbusRTUBuilder::connect()
+{
+    callbackPin() = _enablePin;
+    Serial.print("EN Pin: ");
+    Serial.println(_enablePin);
+    return 1;
+}
+
+uint16_t MockModbusRTUBuilder::read(uint8_t responseIndex)
+{
+    if (responseIndex >= _base.lengthAddress)
+    {
+        Serial.println("Request address outbond");
+        return 0;
+    }
+
+    if (_base.functionCode == 0x03)
+    {
+        Serial.print("Doing read holding: ");
+        Serial.print("address: ");
+        Serial.print(_base.address + responseIndex);
+        Serial.println("");
+    }
+    else if (_base.functionCode == 0x04)
+    {
+        Serial.print("Doing read input: ");
+        Serial.print("address: ");
+        Serial.print(_base.address + responseIndex);
+        Serial.println("");
+    }
+    return 0;
+}
+
+BuilderBaseModbus &MockModbusRTUBuilder::setSlaveId(uint8_t slaveId)
+{
+    _base.slaveId = slaveId;
+    return *this;
+}
+BuilderBaseModbus &MockModbusRTUBuilder::setAddress(uint8_t address)
+{
+    _base.address = address;
+    return *this;
+}
+
+BuilderBaseModbus &MockModbusRTUBuilder::setFunctionCode(uint8_t functionCode)
+{
+    _base.functionCode = functionCode;
+    return *this;
+}
+
+BuilderBaseModbus &MockModbusRTUBuilder::setLengthAddress(uint8_t lengthAddress)
+{
+    _base.lengthAddress = lengthAddress;
+    return *this;
+}
+
+ModbusBase MockModbusRTUBuilder::getConfig()
+{
+    return _base;
+}
--- src/SensorBuilder.h
+++ src/SensorBuilder.h
@@ -2,17 +2,7 @@
 #define SENSOR_MANAGER_H
 
 #include <Arduino.h>
-#include <cstdint>
 #include <ModbusMaster.h>
-#include <memory>
-
-class CommunicationProperties
-{
-public:
-    virtual ~CommunicationProperties() = default;
-    virtual void connect() = 0;
-    virtual uint16_t read(uint8_t responseIndex = 0) = 0;
-};
 
 class ModbusBase
 {
@@ -21,6 +11,15 @@ public:
     uint8_t address;
     uint8_t functionCode;
     uint8_t lengthAddress;
+
+    String toString()
+    {
+        char buffer[37];
+        snprintf(buffer, sizeof(buffer),
+                 "id:%" PRIu8 ", fc:%" PRIu8 ", add:%" PRIu8 ", len:%" PRIu8 "",
+                 slaveId, functionCode, address, lengthAddress);
+        return String(buffer);
+    }
 };
 
 class BuilderBaseModbus
@@ -31,56 +30,21 @@ public:
     virtual BuilderBaseModbus &setAddress(uint8_t address) = 0;
     virtual BuilderBaseModbus &setFunctionCode(uint8_t functionCode) = 0;
     virtual BuilderBaseModbus &setLengthAddress(uint8_t lengthAddress) = 0;
-    virtual std::unique_ptr<ModbusBase> getResult() = 0;
+
+    virtual uint8_t connect() = 0;
+    virtual uint16_t read(uint8_t index) = 0;
+
+    virtual ModbusBase getConfig() = 0;
 };
 
-class ModbusBuilder : public BuilderBaseModbus
+class ModbusRTUBuilder : public BuilderBaseModbus
 {
 private:
-    std::unique_ptr<ModbusBase> base;
+    ModbusBase _base;
 
-public:
-    ModbusBuilder() : base(new ModbusBase()) {}
-
-    ModbusBuilder &setSlaveId(uint8_t slaveId) override
-    {
-        base->slaveId = slaveId;
-        return *this;
-    }
-    ModbusBuilder &setAddress(uint8_t address) override
-    {
-        base->address = address;
-        return *this;
-    }
-    ModbusBuilder &setFunctionCode(uint8_t functionCode) override
-    {
-        base->functionCode = functionCode;
-        return *this;
-    }
-    ModbusBuilder &setLengthAddress(uint8_t lengthAddress) override
-    {
-        base->lengthAddress = lengthAddress;
-        return *this;
-    }
-
-    std::unique_ptr<ModbusBase> getResult() override
-    {
-        return std::move(base);
-    }
-};
-
-class ModbusConnection : public CommunicationProperties
-{
-private:
-    ModbusMaster node;
-    std::unique_ptr<ModbusBase> base;
-    uint8_t enablePin;
-
-    static uint8_t &callbackPin()
-    {
-        static uint8_t pin = 255;
-        return pin;
-    }
+    ModbusMaster _node;
+    HardwareSerial _ss;
+    uint8_t _enablePin;
 
     static void preTransmission()
     {
@@ -92,49 +56,55 @@ private:
         digitalWrite(callbackPin(), LOW);
     }
 
+    static uint8_t &callbackPin()
+    {
+        static uint8_t pin = 255;
+        return pin;
+    }
+
 public:
-    ModbusConnection(uint8_t enablePin) : enablePin(enablePin) {}
+    ModbusRTUBuilder(HardwareSerial &serial, uint8_t enPin);
+    ~ModbusRTUBuilder();
 
-    void setConfig(std::unique_ptr<ModbusBase> config)
+    BuilderBaseModbus &setSlaveId(uint8_t slaveId) override;
+    BuilderBaseModbus &setAddress(uint8_t address) override;
+    BuilderBaseModbus &setFunctionCode(uint8_t functionCode) override;
+    BuilderBaseModbus &setLengthAddress(uint8_t lengthAddress) override;
+
+    uint8_t connect() override;
+    uint16_t read(uint8_t index) override;
+
+    ModbusBase getConfig() override;
+};
+
+class MockModbusRTUBuilder : public BuilderBaseModbus
+{
+private:
+    ModbusBase _base;
+
+    ModbusMaster _node;
+    HardwareSerial _ss;
+    uint8_t _enablePin;
+
+    static uint8_t &callbackPin()
     {
-        base = std::move(config);
+        static uint8_t pin = 255;
+        return pin;
     }
 
-    void connect() override
-    {
-        callbackPin() = enablePin;
-        pinMode(enablePin, OUTPUT);
-        digitalWrite(enablePin, LOW);
-        node.preTransmission(preTransmission);
-        node.postTransmission(postTransmission);
-        node.begin(base->slaveId, Serial);
-    }
+public:
+    MockModbusRTUBuilder(HardwareSerial &serial, uint8_t enPin);
+    ~MockModbusRTUBuilder();
 
-    uint16_t read(uint8_t responseIndex) override
-    {
-        if (responseIndex >= base->lengthAddress)
-        {
-            return 0;
-        }
+    BuilderBaseModbus &setSlaveId(uint8_t slaveId) override;
+    BuilderBaseModbus &setAddress(uint8_t address) override;
+    BuilderBaseModbus &setFunctionCode(uint8_t functionCode) override;
+    BuilderBaseModbus &setLengthAddress(uint8_t lengthAddress) override;
 
-        if (base->functionCode == 0x03)
-        {
-            uint8_t result = node.readHoldingRegisters(base->address, base->lengthAddress);
-            if (result == node.ku8MBSuccess)
-            {
-                return node.getResponseBuffer(responseIndex);
-            }
-        }
-        else if (base->functionCode == 0x04)
-        {
-            uint8_t result = node.readInputRegisters(base->address, base->lengthAddress);
-            if (result == node.ku8MBSuccess)
-            {
-                return node.getResponseBuffer(responseIndex);
-            }
-        }
-        return 0;
-    }
+    uint8_t connect() override;
+    uint16_t read(uint8_t index) override;
+
+    ModbusBase getConfig() override;
 };
 
 #endif
