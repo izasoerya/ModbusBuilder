3337a1c4c2a36e988ab87c16efa8f6489c5c801a
izasoerya <ihza.surya@gmail.com>
2026-06-08 17:03:15 +0700

    chore: update example to latest

--- example/example.cpp
+++ example/example.cpp
@@ -1,18 +1,25 @@
 #include <Arduino.h>
-#include "SensorBuilder.h"
+#include <SensorBuilder.h>
+
+#define EN_PIN 3
+
+ModbusRTUBuilder modbusRTU(Serial2, EN_PIN);
 
 void setup()
 {
-    ModbusBuilder shtObject;
-    shtObject.setSlaveId(1).setFunctionCode(0x03).setAddress(0x00).setLengthAddress(2);
+    Serial.begin(115200);
 
-    ModbusConnection shtConnection(13); // 13 is DE / RE PIN
-    shtConnection.setConfig(shtObject.getResult());
-    shtConnection.connect();
-    uint16_t result = shtConnection.read();
+    modbusRTU.setSlaveId(0x01).setFunctionCode(0x03).setAddress(0x02).setLengthAddress(2);
+    modbusRTU.connect();
+
+    Serial.println(modbusRTU.getConfig().toString());
 }
 
 void loop()
 {
-    // put your main code here, to run repeatedly:
+
+    float temperature = modbusRTU.read(0); // Read first register
+    float humidity = modbusRTU.read(1);    // Read second register
+
+    delay(2000);
 }
